b1a7ec07ae4bccdde94e571177d5d62fea02a7c1
tigist_alemu <tigist_alemu@tigistalemusAir.home>
2021-10-24 17:38:23 -0400

    wiki

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=se
 const btn = document.querySelector('.btn')
 const output = document.querySelector('.output')
 const inputval = document.querySelector('.val')
-
+const box  = document.querySelector('.box')
 let attemptCounter = false
 inputval.value = 'hello'
 btn.textContent = "Load JSON"
@@ -12,6 +12,18 @@ btn.addEventListener('click', () => {
     fetch(url)
      .then(res => res.json())
      .then(data => {
-         console.log(data)
+         dataMaker(data.query)
      })
 })
+
+
+const dataMaker = (content) => {
+    output.innerHTML = "Result for"
+  content.search.forEach(lineContent => {
+      const div = document.createElement('div')
+            div.classList.add('box')
+            div.innerHTML = `${lineContent.title}`
+            div.innerHTML = `${lineContent.snippet}`
+            output.append(div)
+  })
+}
